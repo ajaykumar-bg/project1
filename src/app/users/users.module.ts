f4accfa04e195642082fe44bd9f501b0078ea3f7
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-01 14:31:58 +0530

    Added a component 'user-list' to the feature module 'users'

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: []
+  declarations: [UserListComponent]
 })
 export class UsersModule { }
