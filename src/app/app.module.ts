7e4d0d8de0e54e5a9e72f3d3521118b7a814e5d1
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-01 12:43:00 +0530

    Added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core';
+import { SharedModule } from './shared';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
