a94c66347e9ad47a62b2f1e7a03a561931542509
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-01 14:36:13 +0530

    Added a component to the feature module tasks

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TasksRoutingModule
   ],
-  declarations: []
+  declarations: [TaskListComponent]
 })
 export class TasksModule { }
