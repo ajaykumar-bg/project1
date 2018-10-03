269be8f8ff977f8e984bdd943875f619e3da33f5
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-03 12:42:17 +0530

    Fix for routerLink path in user - detail redirection

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -6,7 +6,7 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', component: UserListComponent},
-  { path: '/detail/:id', component: UserDetailComponent },
+  { path: 'detail/:id', component: UserDetailComponent },
 ];
 
 @NgModule({
