9a101e6f70feed06d6380aa762bc79270a7d9eb5
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-03 12:11:08 +0530

    Added user edit component and user edit component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: [UserListComponent]
+  declarations: [UserListComponent, UserDetailComponent, UserEditComponent]
 })
 export class UsersModule { }
