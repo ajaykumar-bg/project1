69ec4014824cdf79c28a6bf218c45a5d689b0f42
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-03 12:14:49 +0530

    removed user edit component

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: UserListComponent
-  }
+  { path: '', component: UserListComponent},
+  { path: 'detail/:id', component: UserDetailComponent },
 ];
 
 @NgModule({
