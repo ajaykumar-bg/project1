ad6a7b2f2f0ead4b891563956a877c8f4e35d1ec
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-03 16:31:51 +0530

    Moved in memory data service to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,10 @@ import { CoreModule } from './core';
 import { SharedModule } from './shared';
 import { MaterialModule } from './material.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -34,7 +38,15 @@ import { AppRoutingModule } from './app-routing.module';
     CoreModule,
     SharedModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-
 
 import { MaterialModule } from '../material.module';
 import { LoaderComponent } from './loader/loader.component';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from '../in-memory-data.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule,
-    HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    MaterialModule
   ],
   declarations: [LoaderComponent],
   exports: [LoaderComponent]
