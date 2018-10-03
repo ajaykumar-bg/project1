dbb8c420bcb45e4db3508e362543146352095cc6
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-03 17:40:29 +0530

    Added task detail page

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Task } from '../task';
+import { TaskService } from '../../task.service';
 
 @Component({
   selector: 'app-task-detail',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskDetailComponent implements OnInit {
 
-  constructor() { }
+  editTaskForm: FormGroup;
+  @Input() task: Task;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.editTaskForm = this.fb.group({
+      id: ['', [
+      ]],
+      name: ['', [
+        Validators.required
+      ]]
+    });
+    this.getTask();
+  }
+
+  getTask(): void {
+    const taskId = +this.route.snapshot.paramMap.get('id');
+    this.taskService.getTask(+taskId)
+      .subscribe( data => {
+        this.editTaskForm.setValue(data);
+      });
+  }
+
+  goBack(): void {
+    this.router.navigate(['tasks']);
+  }
+
+ save(): void {
+    this.taskService.updateTask(this.editTaskForm.value)
+      .subscribe(() => this.goBack());
   }
 
 }
