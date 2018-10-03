ffde731516828aff9523d876353b60167d7c5d58
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-03 11:57:48 +0530

    Added @fortawesome/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, fab);
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { CoreModule } from './core';
 import { SharedModule } from './shared';
 import { MaterialModule } from './material.module';
@@ -9,12 +17,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
     CoreModule,
     SharedModule,
     MaterialModule,
