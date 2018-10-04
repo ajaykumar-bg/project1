49988f99841c286a6098fdefffbd4cfbdf3bc7fb
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-10-04 10:38:02 +0530

    Added material side navbar using ng generate command ng generate @angular/material:material-nav --name <component-name>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AppRoutingModule } from '../app-routing.module';
 import { MaterialModule } from '../material.module';
 import { LoaderComponent } from './loader/loader.component';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
-  declarations: [LoaderComponent],
-  exports: [LoaderComponent]
+  declarations: [
+    LoaderComponent,
+    SideNavbarComponent
+  ],
+  exports: [
+    LoaderComponent,
+    SideNavbarComponent
+  ]
 })
 export class SharedModule { }
